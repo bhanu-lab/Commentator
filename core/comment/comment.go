5b1c2140c24006cd05ed8f750f04da7a89a83c1b
bhanu-lab <bhanuputtareddy@gmail.com>
2021-12-28 11:13:44 +0530

    implemented comment service

--- core/comment/comment.go
+++ core/comment/comment.go
@@ -1,6 +1,8 @@
 package comment
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -23,13 +25,60 @@ type Comment struct {
 	Slug    string
 	Body    string
 	Author  string
-	Created time.time
+	Created time.Time
 }
 
 type CommentService interface {
 	GetComment(ID uint) (Comment, error)
 	GetAllComments() ([]Comment, error)
-	PostComment(comment Comment) (Comment, error)
+	PostComment(comment Comment) (bool, error)
 	UpdateComment(ID uint, comment Comment) (Comment, error)
 	DeleteComment(ID uint)
+}
+
+//GetComment - retrieves comment from the ID given
+func (s *Service) GetComment(ID uint) (Comment, error) {
+	var comment Comment
+	if res := s.DB.First(&comment, ID); res.Error != nil {
+		return Comment{}, res.Error
+	}
+	return comment, nil
+}
+
+//GetAllComments - retrieves all comments present
+func (s *Service) GetAllComments() ([]Comment, error) {
+	var comments []Comment
+	if res := s.DB.Find(&comments); res.Error != nil {
+		return comments, res.Error
+	}
+	return comments, nil
+}
+
+//PostComment - adds new comment
+func (s *Service) PostComment(comment Comment) (bool, error) {
+	if res := s.DB.Save(&comment); res.Error != nil {
+		return false, res.Error
+	}
+	return true, nil
+}
+
+//UpdateComment - updates comment for the ID given
+func (s *Service) UpdateComment(ID uint, newcomment Comment) (Comment, error) {
+	var comment Comment
+	if res := s.DB.Find(&comment, ID); res.Error != nil {
+		return Comment{}, res.Error
+	}
+
+	if res := s.DB.Model(comment).Update(newcomment); res.Error != nil {
+		return Comment{}, res.Error
+	}
+	return comment, nil
+}
+
+//DeleteComment - deletes comment based on ID given
+func (s *Service) DeleteComment(ID uint) error {
+	if res := s.DB.Delete(&Comment{}, ID); res.Error != nil {
+		return res.Error
+	}
+	return nil
 }
